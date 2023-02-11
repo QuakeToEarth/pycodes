8f6f01cc5355b2099363ccb9f921684b5bee4fdb
QuakeToEarth <73459097+QuakeToEarth@users.noreply.github.com>
2023-02-11 18:27:53 +1100

    Add files via upload

---
+++ forloop1.py
@@ -0,0 +1,11 @@
+# # # for i in range(1,11):
+# # #     print("Hello World")
+# # for i in range(1,21):
+# #     print(i)
+# for i in range(1,101):
+#     if (i%2 ==0):
+#         print(i)
+# Generating random numbers from 1-10
+import random
+n = random.randint(1,10)
+print(n)
---
+++ forloop2.py
@@ -0,0 +1,11 @@
+#Printing the multiples of 5
+# for i in range(5,51):
+#     if (i%5==0):
+#         print(i)
+# for i in range(5,51,5):
+#     print(i)
+#Displaying all buzz numbers from 1-100
+#A number disiable by 7 or the last digit is 7
+# for i in range (1,101):
+#     if (i%7==0 or i%10 ==7):
+#         print(i)
